fff543f3259c9e014acc20c2db4058e348d78078
kislay0298 <kisalaykomal2767@gmail.com>
2025-03-28 15:07:41 +0530

    updated dependency

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
